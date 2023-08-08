4e63a3919ab0487263565e6daba7c0feab4bf588
Lucas Arlego <49014111+lucasarlego@users.noreply.github.com>
2023-08-08 15:08:32 -0300

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -2,29 +2,29 @@ import tkinter as tk
 from tkinter import filedialog
 import pandas as pd
 
-class CSVtoXLSXConverterApp:
+class ConverterApp:
     def __init__(self, root):
         self.root = root
         self.root.title("CSV to XLSX Converter")
-        self.root.geometry("700x200")  # Ajuste o tamanho da janela conforme necessário
+        self.root.geometry("700x200")
 
         self.file_path = ""
         self.status_var = tk.StringVar()
-        self.conversion_status_var = tk.StringVar()  # Variável para a mensagem de status da conversão
+        self.conversion_status_var = tk.StringVar()
 
         self.path_frame = tk.Frame(self.root)
         self.path_frame.pack(fill=tk.X, padx=20, pady=(20, 10))
 
-        self.path_label = tk.Label(self.path_frame, text="Diretório:")
+        self.path_label = tk.Label(self.path_frame, text="Folder:")
         self.path_label.pack(side=tk.LEFT, padx=(0, 10))
 
         self.path_entry = tk.Entry(self.path_frame, textvariable=self.status_var)
         self.path_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
 
-        self.choose_button = tk.Button(self.path_frame, text="Escolher Arquivo", command=self.choose_csv_file)
+        self.choose_button = tk.Button(self.path_frame, text="Search File", command=self.choose_csv_file)
         self.choose_button.pack(side=tk.LEFT, padx=(10, 0))
 
-        self.convert_button = tk.Button(self.root, text="Converter para XLSX", command=self.convert_csv_to_xlsx)
+        self.convert_button = tk.Button(self.root, text="Convert to XLSX", command=self.convert_csv_to_xlsx)
         self.convert_button.pack(padx=20, pady=(10, 0))
 
         self.conversion_status_label = tk.Label(self.root, textvariable=self.conversion_status_var)
@@ -41,9 +41,9 @@ class CSVtoXLSXConverterApp:
             output_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel Files", "*.xlsx")])
             if output_path:
                 df.to_excel(output_path, index=False)
-                self.conversion_status_var.set("Conversão concluída!")
+                self.conversion_status_var.set("Conversion Complete!")
 
 if __name__ == "__main__":
     app_root = tk.Tk()
-    app = CSVtoXLSXConverterApp(app_root)
+    app = ConverterApp(app_root)
     app_root.mainloop()
